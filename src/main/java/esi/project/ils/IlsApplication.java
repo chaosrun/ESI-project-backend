11f799724b42c5e103b00234b5c1d11a23c65823
Zhaosi Qu <git@chaos.run>
2022-04-27 21:33:08 +0300

    Add hello world controller

--- src/main/java/esi/project/ils/IlsApplication.java
+++ src/main/java/esi/project/ils/IlsApplication.java
@@ -3,10 +3,20 @@ package esi.project.ils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 @PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class IlsApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IlsApplication.class, args);
