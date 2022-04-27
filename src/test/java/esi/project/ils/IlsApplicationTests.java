02e804b8ebd6d9ab651fc6047e50fab59b2bb6a6
Zhaosi Qu <git@chaos.run>
2022-04-27 20:25:44 +0300

    Initiate the Gradle project

---
+++ src/test/java/esi/project/ils/IlsApplicationTests.java
@@ -0,0 +1,13 @@
+package esi.project.ils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
