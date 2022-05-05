2d41062e6eb758840b93d7bc345d661017903dc0
Timothy Fadayini <timothy33.tf@gmail.com>
2022-05-05 08:51:26 +0300

    added ability to delete, update and get one user

--- src/main/java/esi/project/ils/users/UserService.java
+++ src/main/java/esi/project/ils/users/UserService.java
@@ -3,6 +3,10 @@ package esi.project.ils.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -10,5 +14,29 @@ public class UserService {
 
     public User addUser(User user) {
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    public Optional<User> getUserWithId(int id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> updateUser(int id, User updatedUserInformation) {
+        return userRepository.findById(id).map(user -> {
+            user.setName(updatedUserInformation.getName());
+            user.setEmail(updatedUserInformation.getEmail());
+            user.setPassword(updatedUserInformation.getPassword());
+            user.setRole(updatedUserInformation.getRole());
+            return userRepository.save(user);
+        });
+    }
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
     }
 }
