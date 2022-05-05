30a1366b19feb37216757853f87b2034558a9c94
Zhaosi Qu <git@chaos.run>
2022-05-05 10:02:14 +0300

    Resolve rebase conflicts

--- src/main/java/esi/project/ils/ErrorHandling/ValidationErrorResponse.java
+++ src/main/java/esi/project/ils/ErrorHandling/ValidationErrorResponse.java
@@ -1,11 +1,36 @@
 package esi.project.ils.ErrorHandling;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ValidationErrorResponse {
 
     private List<Violation> violations = new ArrayList<>();
+    private int statusCode;
+    private Date timestamp;
+
+    public ValidationErrorResponse(int statusCode, Date timestamp, List<Violation> violations) {
+        this.statusCode = statusCode;
+        this.timestamp = timestamp;
+        this.violations = violations;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public List<Violation> getViolations() {
         return violations;
