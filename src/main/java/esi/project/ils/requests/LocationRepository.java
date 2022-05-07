e81e19cf59abfca181dc7df4a3f58989013710f1
Zhaosi Qu <git@chaos.run>
2022-05-08 01:33:08 +0300

    Add location data entity

---
+++ src/main/java/esi/project/ils/requests/LocationRepository.java
@@ -0,0 +1,11 @@
+package esi.project.ils.requests;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+    Optional<Location> findById(int id);
+}
