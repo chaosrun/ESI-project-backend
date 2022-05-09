229568383869232d72fe46a6d87a5a12af30ff00
Zhaosi Qu <git@chaos.run>
2022-05-09 21:02:43 +0300

    Fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO users (home_library, email, name, password, role) VALUES ('A', 'libr
 INSERT INTO users (home_library, email, name, password, role) VALUES ('B', 'Bborrower@example.com', 'Borrower B', '$2a$10$glHBb0tkb6AP0fS6SnKKbOtW4RZLnK51GoISvaB3sADZ73ENERR9K', 'BORROWER');
 INSERT INTO users (home_library, email, name, password, role) VALUES ('B', 'Blibrarian@example.com', 'Librarian B', '$2a$10$zYUcORHL0AxOdCq9tdhIiO0mjszwHAE7tX/uC/.BEJEOk/gtd1AOm', 'LIBRARIAN');
 
-INSERT INTO materials (home_library, title, author, call_number, published_at) VALUES ('A', 'The Art of Computer Programming', 'Donald Knuth', '12345', '1962-01-01');
-INSERT INTO materials (home_library, title, author, call_number, published_at) VALUES ('A', 'Fundamentals of Algorithms', 'Donald Knuth', '22222', '1962-01-01');
-INSERT INTO materials (home_library, title, author, call_number, published_at) VALUES ('B', 'A Brief History of Time', 'Stephen Hawking', '54321', '1988-01-01');
-INSERT INTO materials (home_library, title, author, call_number, published_at) VALUES ('B', 'The Universe in a Nutshell', 'Stephen Hawking', '33333', '1988-01-01');
+INSERT INTO materials (home_library, title, author, type, call_number, published_at) VALUES ('A', 'The Art of Computer Programming', 'Donald Knuth', 'book', '12345', '1962-01-01');
+INSERT INTO materials (home_library, title, author, type, call_number, published_at) VALUES ('A', 'Fundamentals of Algorithms', 'Donald Knuth', 'book', '22222', '1962-01-01');
+INSERT INTO materials (home_library, title, author, type, call_number, published_at) VALUES ('B', 'A Brief History of Time', 'Stephen Hawking', 'book', '54321', '1988-01-01');
+INSERT INTO materials (home_library, title, author, type, call_number, published_at) VALUES ('B', 'The Universe in a Nutshell', 'Stephen Hawking', 'book', '33333', '1988-01-01');
